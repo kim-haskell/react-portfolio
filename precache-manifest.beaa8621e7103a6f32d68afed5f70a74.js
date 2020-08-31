73fb3ba3e96ed3a9ee6fc24c069021a0c97aed04
Kim Haskell <kim.haskell03@gmail.com>
2020-08-31 16:47:33 -0600

    Updates

---
+++ precache-manifest.beaa8621e7103a6f32d68afed5f70a74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "972dfcb94f8a8345dce5aa492113aaa1",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "b8a869a2f27d7b0d0984",
+    "url": "/react-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ee5af798be051ec3a174",
+    "url": "/react-portfolio/static/js/2.49de80ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.49de80ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8a869a2f27d7b0d0984",
+    "url": "/react-portfolio/static/js/main.1148fd7b.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
