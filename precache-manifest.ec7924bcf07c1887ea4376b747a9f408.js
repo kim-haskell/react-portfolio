a7fae995360f379a18db15f3bb6b1a5ec3578aea
Kim Haskell <kim.haskell03@gmail.com>
2020-09-02 17:33:07 -0600

    Updates

---
+++ precache-manifest.ec7924bcf07c1887ea4376b747a9f408.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384d7addbb15a1225fb178dbbfc7a601",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "27c1c96e7b1c815cd77d",
+    "url": "/react-portfolio/static/css/main.9729bd13.chunk.css"
+  },
+  {
+    "revision": "baa179d2855b82a1f9a0",
+    "url": "/react-portfolio/static/js/2.f6c4254e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.f6c4254e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27c1c96e7b1c815cd77d",
+    "url": "/react-portfolio/static/js/main.3bc39dc4.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "7fbc515e970dd3dd9de1d73b2a3aee34",
+    "url": "/react-portfolio/static/media/About_Me.7fbc515e.png"
+  },
+  {
+    "revision": "1d15045b60976e73c2ba9c3b9873bb00",
+    "url": "/react-portfolio/static/media/Hungry.1d15045b.png"
+  },
+  {
+    "revision": "81b098405c0229b101cee346c8fccfe8",
+    "url": "/react-portfolio/static/media/NeighBoard.81b09840.png"
+  },
+  {
+    "revision": "b991fd311af83ea332d6a1c5cfd034b8",
+    "url": "/react-portfolio/static/media/Note-Taker.b991fd31.png"
+  },
+  {
+    "revision": "e2a2d962ef6647087e57c03948137dc5",
+    "url": "/react-portfolio/static/media/Resume.e2a2d962.pdf"
+  },
+  {
+    "revision": "d7506f4eca58d6345b7c46313473440d",
+    "url": "/react-portfolio/static/media/WeatherDashboard.d7506f4e.png"
+  },
+  {
+    "revision": "981babbeff221065c1508df78b73fb23",
+    "url": "/react-portfolio/static/media/WorkDaySchedule.981babbe.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.565dbb305e177157b2164b9b822c2078.js"
+  "/react-portfolio/precache-manifest.ec7924bcf07c1887ea4376b747a9f408.js"
 );
 
 self.addEventListener('message', (event) => {
